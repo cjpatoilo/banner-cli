361ef91b148820a6a9ddfad0c9cfdf0d4066f8c0
CJ Patoilo <cjpatoilo@gmail.com>
2016-12-01 09:53:07 -0300

    Release v0.6.0

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const fs = require('fs')
-const path = require('path')
-const prependFile = require('prepend-file')
+const fs = require('fs');
+const path = require('path');
+const prependFile = require('prepend-file');
 
 function add(argv) {
-	const info = require(`${process.cwd()}/package.json`) || ' '
-	const name = info.name.charAt(0).toUpperCase() + info.name.slice(1) || 'Project name'
-	const version = info.version || '0.0.0'
-	const homepage = info.homepage || 'http://your-homepage.com'
-	const license = info.license || 'your'
-	const author = info.author.split('<')[0].trim() || 'Author name'
-	const year = new Date().getFullYear()
-	const banner =  `/*!\n * ${name} v${version}\n * ${homepage}\n *\n * Copyright (c) ${year} ${author}\n * Licensed under the ${license} license\n*/\n\n`
+	const info = require(`${process.cwd()}/package.json`);
+	const name = info.name.charAt(0).toUpperCase() + info.name.slice(1) || 'Project name';
+	const version = info.version || '0.0.0';
+	const homepage = info.homepage || 'http://your-homepage.com';
+	const license = info.license || 'your';
+	const author = info.author.split('<')[0].trim() || 'Author name';
+	const year = new Date().getFullYear();
+	const banner =  `/*!\n * ${name} v${version}\n * ${homepage}\n *\n * Copyright (c) ${year} ${author}\n * Licensed under the ${license} license\n */\n\n`;
 	if (!fs.existsSync(path.dirname(argv[0]))) {
-		console.log(`${argv[0]} Not found!`)
-		process.exit(1)
+		console.log(`${argv[0]} Not found!`);
+		process.exit(1);
 	}
 
 	argv.map(file => {
@@ -21,7 +21,7 @@ function add(argv) {
 			prependFile(file, banner, (error) => {
 				if (error) {
 					console.log(`${file} Not found!`);
-					process.exit(1)
+					process.exit(1);
 				}
 			})
 		}
