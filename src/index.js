1219874b1bb31bc1b386cc72de25bed09de2a722
sylvainpolletvillard <sylvain.pollet.villard@gmail.com>
2017-03-09 00:49:39 +0100

    use glob for sourcefiles

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 'use strict'
+const glob = require('glob')
 const prependFile = require('prepend-file')
 const pkg = require(`${process.cwd()}/package.json`)
 
@@ -7,23 +8,26 @@ function banner (options) {
 	options.name = options.name || pkg.name || 'unknown'
 	options.tag = options.tag || pkg.version || '0.0.0'
 	options.homepage = options.homepage || pkg.homepage || `https://npm.com/${options.name}`
-	options.license = options.license || pkg.license || 'unknown'
+	options.license = options.license || pkg.license
 	options.author = options.author || pkg.author.split('<')[0].trim() || ''
 	options.year = options.year || new Date().getFullYear()
 
-	const template = `/*!
+	const template = options.template || `/*!
  * ${options.name.charAt(0).toUpperCase() + options.name.slice(1)} v${options.tag}
  * ${options.homepage}
  *
  * Copyright (c) ${options.year} ${options.author}
- * Licensed under the ${options.license} license
- */\n
+ *${options.license ? ` Licensed under the ${options.license} license\n *` : ''}/\n
 `
 
-	if (!options.source) console.log(`File not found!`)
-	else options.source.map(file => prependFile(file, template))
-
-	process.exit(0)
+	if (!options.source) throw new Error(`File not found!`)
+	else {
+		glob(options.source, (err, files) => {
+			if (err) throw err
+			files.map(file => prependFile(file, template))
+			process.exit(0)
+		})
+	}
 }
 
 module.exports = banner
